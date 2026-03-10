05f6ed17240d87cda682575c28fdb9f702b6f4db
Jared Salzano <jaredsalzano@gmail.com>
2026-03-10 17:25:23 -0400

    404 (#65)

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,24 +1,18 @@
 import Link from "next/link";
-import Footer from "@/components/Footer";
 
 export default function NotFound() {
   return (
-    <>
-      <main className="relative bg-white/90 min-h-[80vh] flex items-center">
-        <div className="max-w-4xl mx-auto px-10 sm:px-16 py-24">
-          <div className="prose prose-lg">
-            <h1 className="mt-0">Page not found</h1>
-            <p>
-              The page you&apos;re looking for doesn&apos;t exist or may have
-              moved.
-            </p>
-            <Link href="/">Back to home</Link>
-          </div>
+    <main className="relative bg-white/90 min-h-screen flex items-center">
+      <div className="max-w-4xl mx-auto px-10 sm:px-16 py-24">
+        <div className="prose prose-lg">
+          <h1 className="mt-0 not-prose text-4xl font-black">Page not found</h1>
+          <p>
+            The page you&apos;re looking for doesn&apos;t exist or may have
+            moved.
+          </p>
+          <Link href="/">Back to home</Link>
         </div>
-      </main>
-      <div className="relative bg-white/90">
-        <Footer />
       </div>
-    </>
+    </main>
   );
 }
